4f8fff7d58337a1446fb3a36e41e492288ce2f71
elisheva1885 <elisheva1885@gmail.com>
2026-03-15 11:30:26 +0200

    the db details on an object

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,21 +2,28 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-interface Config {
-    port: number;
+interface DB {
     dbHost: string;
     dbPort: number;
     dbUsername: string;
     dbPassword: string;
     dbDatabase: string
+}
+
+interface Config {
+    port: number;
+    db: DB
 
 }
 
 const config: Config = {
     port: Number(process.env.PORT) || 3000,
-    dbHost: process.env.DB_HOST || 'localhost',
+    db : { dbHost: process.env.DB_HOST || 'localhost',
     dbPort: Number(process.env.DB_PORT) || 5432,
     dbUsername: process.env.DB_USERNAME || 'postgres',
     dbPassword: process.env.DB_PASSWORD || '',
     dbDatabase: process.env.DB_DATABASE || 'default_db_name',
+    
+
+    }
 }
