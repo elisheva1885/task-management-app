99d9e19683fa7bfc91c8bfb4987638181e3193fe
elisheva1885 <elisheva1885@gmail.com>
2026-03-17 11:14:08 +0200

    updating the route

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -3,4 +3,4 @@ import { TaskController } from '../controllers/task.controller.js';
 const taskRouter = express.Router()
 const taskController = new TaskController();
 
-taskRouter.post('/register', taskController.getAllTasks)
+taskRouter.post('/', taskController.getAllTasks)
