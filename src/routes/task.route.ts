0c83a21083959c5db26b34ea0b33a3a0defe95c2
elisheva1885 <elisheva1885@gmail.com>
2026-03-17 11:20:54 +0200

    add the route

---
+++ src/routes/task.route.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+import { TaskController } from '../controllers/task.controller.js';
+const taskRouter = express.Router()
+const taskController = new TaskController();
+
+
+taskRouter.post('/', taskController.addTask)
