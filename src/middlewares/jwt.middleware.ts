98d022f862e09ed61e93a2722c913d4c8bf2fb12
elisheva1885 <elisheva1885@gmail.com>
2026-03-19 12:31:13 +0200

    fixing the comment

--- src/middlewares/jwt.middleware.ts
+++ src/middlewares/jwt.middleware.ts
@@ -10,16 +10,18 @@ export const authentification = (
 ) => {
     const header = req.headers.authorization;
     if (!header) {
-        res.status(401).json({ message: "Unauthorized" })
+        return res.status(401).json({ message: "Unauthorized" })
     }
     const token = header!.split(" ")[1];
     if (!token) {
-        res.status(401).json({ message: "Unauthorized" })
+        return res.status(401).json({ message: "Unauthorized" })
     }
-    const decode = jwt.verify(token!, configData.jwt)
-    if (!decode) {
-        res.status(401).json({ message: "Unauthorized" })
+    try {
+        const decode = jwt.verify(token!, configData.jwt)
+        req.currentUser = decode;
+        next();
+
+    }catch (err) {
+        return res.status(401).json({ message: "Unauthorized" })
     }
-    req.currentUser = decode;
-    next();
 }
