0f3dcc5bd5ac85047de45cd5fd129c5915666144
elisheva1885 <elisheva1885@gmail.com>
2026-03-22 21:08:31 +0200

    fixing some errors

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,5 +1,5 @@
+import jwt from 'jsonwebtoken'
 import type { CreateTaskRequestDto } from "../dto/create-task.dto.js";
-import type { Task } from "../entities/Task.entity.js";
 import { TaskService } from "../services/task.service.js";
 import { type Response, type Request } from "express";
 import type { AuthRequest } from "../types/auth.types.js";
@@ -8,12 +8,14 @@ const taskService = new TaskService()
 export class TaskController {
     async addTask(req: AuthRequest, res: Response) {
         const data: CreateTaskRequestDto = req.body;
-        const userId = (req.currentUser as Jwt.JwtPayload).userId;
-
+        const userId = (req.currentUser as jwt.JwtPayload).userId;
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
         if (!data.title || !data.description || !data.priority || !data.deadline) {
             return res.status(400).json({ message: "All details required" });
         }
-        if (typeof data.title !== "string" || typeof data.description !== "string" || typeof data.priority !== "string" ||  data.deadline !== "string") {
+        if (typeof data.title !== "string" || typeof data.description !== "string" || typeof data.priority !== "string" || typeof data.deadline !== "string") {
             return res.status(400).json({ message: "Invalid input" });
         }
         const task = await taskService.addTask(data, userId);
--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -1,8 +1,8 @@
-import express from 'express'
+import express, { type RequestHandler } from 'express'
 import { TaskController } from '../controllers/task.controller.js';
 import { authentication } from '../middlewares/jwt.middleware.js';
 export const taskRouter = express.Router()
 const taskController = new TaskController();
 
-
-taskRouter.post('/',authentication , taskController.addTask)
+taskRouter.use(authentication as RequestHandler)
+taskRouter.post('/' , taskController.addTask as RequestHandler)
