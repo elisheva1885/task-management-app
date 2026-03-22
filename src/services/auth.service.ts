5e7ebc18b924b9114715fc2cc4d103c3499ff895
elisheva1885 <elisheva1885@gmail.com>
2026-03-22 14:05:28 +0200

    fixing comments

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,20 +5,23 @@ import { AppError } from "../errors/app-errors.js";
 import { encryptionPassword } from "../helpers/password-helper.js"
 
 const userRepository = AppDataSource.getRepository(User);
-
+const getUserByUsername = async(username: string) => {
+    const existUser = await userRepository.findOne({
+        where: { username }
+    })
+    return existUser;
+}
 
 export class AuthService {
-    async register(username: string, password: string):Promise<RegisterResponseDto> {
-        const existUser = await userRepository.findOne({
-            where: { username: username }
-        })
+    async register(username: string, password: string): Promise<RegisterResponseDto> {
+        const existUser = await getUserByUsername(username);
         if (existUser) {
-            throw new AppError("User already exist",400)
+            throw new AppError("User already exist", 400)
         }
         const hashPass = encryptionPassword(password);
-        const user : User = userRepository.create({ username,password:  hashPass })
+        const user: User = userRepository.create({ username, password: hashPass })
         await userRepository.save(user);
-        return {id: user.id ,  username: user.username}
+        return { id: user.id, username: user.username }
     }
 
 }
