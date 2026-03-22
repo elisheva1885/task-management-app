ed588a0029f820d8c9bfcd06e0533d8a20d5f357
elisheva1885 <elisheva1885@gmail.com>
2026-03-23 01:16:01 +0200

    checkout that the get works

--- src/entities/Task.entity.ts
+++ src/entities/Task.entity.ts
@@ -7,19 +7,19 @@ export class Task {
     @PrimaryGeneratedColumn("uuid")
     id!: string;
 
-    @Column({ nullable: false })
+    @Column({ nullable: false , type: "varchar" })
     title!: string;
 
-    @Column({ nullable: false })
+    @Column({ nullable: false , type: "varchar" })
     description!: string;
 
-    @Column({ nullable: false })
+    @Column({ nullable: false , type: "varchar" })
     priority!: string;
 
     @Column({ type: "timestamp", nullable: false })
     deadline!: Date;
 
-    @Column()
+    @Column({ type: "varchar"})
     userId!: string;
 
     @ManyToOne(() => User, user => user.tasks)
--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -1,6 +1,9 @@
-import express from 'express'
+import express, { type RequestHandler } from 'express'
 import { TaskController } from '../controllers/task.controller.js';
+import { authentication } from '../middlewares/jwt.middleware.js';
 export const taskRouter = express.Router()
 const taskController = new TaskController();
 
+taskRouter.use(authentication as RequestHandler)
 taskRouter.get('/', taskController.getAllTasks)
+
