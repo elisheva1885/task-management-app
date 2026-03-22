6978688e3340db87265e3056a95d2f8a00c4c19b
elisheva1885 <elisheva1885@gmail.com>
2026-03-22 21:13:43 +0200

    creating type UserInfo

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -2,11 +2,16 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { configEnvironmentData } from '../config/config.js';
+type UserInfo = {
+    id: string;
+    username: string
+}
+
 export const comparePassword = async(clinetPass : string , dbPass : string) : Promise<Boolean>=> {
     const match = await bcrypt.compare(clinetPass, dbPass)
     return match;
 }
 
-export const generateToken= (userInfo : Object) :  string=> {
+export const generateToken= (userInfo : UserInfo) :  string=> {
     return jwt.sign(userInfo, configEnvironmentData.jwt, {expiresIn:"1d"})
 }
