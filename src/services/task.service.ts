b98c9ca88775ad0d38975d1e9893ad6ae66a6da0
elisheva1885 <elisheva1885@gmail.com>
2026-03-23 09:37:31 +0200

    use app error

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,20 +1,24 @@
-import { AppDataSource } from "../app.js";
+import { AppDataSource } from "../db/data-source";
+import { Task } from "../entities/Task.entity";
+import { AppError } from "../errors/app-errors";
+
 
 const taskRepository = AppDataSource.getRepository(Task);
 
 
 export class TaskService {
-    async updateTask( taskData : Partial<Task>,id: number) {
-        const task = taskRepository.find({
-            where: {id: id}
+    async updateTask( taskData : Partial<Task>,id: string, userId: string) {
+
+        const task = await taskRepository.findOne({
+           where: {id, userId}
     });
-        if(!task){
-            throw new Error("task not found")
+        if(typeof(task) == undefined){
+            throw new AppError("task not found", 404)
         }
         task.title = taskData.title;
-        task.description = taskData.description;
-        task.priority = taskData.priority;
-        task.deadline = taskData.deadline;
+        task?.description = taskData.description;
+        task?.priority = taskData.priority;
+        task?.deadline = taskData.deadline;
 
         await taskRepository.save(task);
         return task;
