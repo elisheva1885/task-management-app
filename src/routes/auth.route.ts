4529119029737e8c1aad60401801e03491c1473e
elisheva1885 <elisheva1885@gmail.com>
2026-03-23 12:33:05 +0200

    add zod validation

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import { AuthController } from '../controllers/auth.controller.js'
 import asyncHandler from '../middlewares/async-handler.middleware.js';
+import { validate } from '../middlewares/validate.middleware.js';
+import { registerSchema } from '../schemas/auth.schema.js';
 export const authRouter = express.Router()
 const authController = new AuthController();
 
-authRouter.post('/register', asyncHandler(authController.register));
+authRouter.post('/register',validate(registerSchema) ,asyncHandler(authController.register));
