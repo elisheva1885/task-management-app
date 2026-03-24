28ea46b5581618e82fe06dc939ba0a69b63cf896
elisheva1885 <elisheva1885@gmail.com>
2026-03-24 10:59:19 +0200

    add asyncHandler

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -3,8 +3,9 @@ import { TaskController } from '../controllers/task.controller.js';
 import { authentication } from '../middlewares/jwt.middleware.js';
 import { validate } from '../middlewares/validate.middleware.js';
 import { createTaskSchema } from '../schemas/task.schema.js';
+import asyncHandler from '../middlewares/async-handler.middleware.js'
 export const taskRouter = express.Router()
 const taskController = new TaskController();
 
-taskRouter.use(authentication as RequestHandler)
-taskRouter.post('/' ,validate(createTaskSchema), taskController.addTask as RequestHandler)
+taskRouter.use(authentication)
+taskRouter.post('/' ,validate(createTaskSchema), asyncHandler(taskController.addTask))
