edf08133a7f18858d270a8c46eff50dcc24242df
elisheva1885 <elisheva1885@gmail.com>
2026-03-24 11:34:58 +0200

    check the flow for add task

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -8,13 +8,7 @@ const taskService = new TaskService()
 export class TaskController {
     async addTask(req: AuthRequest, res: Response) {
         const data: CreateTaskRequestDto = req.body;
-        const userId = (req.currentUser as jwt.JwtPayload)?.id;
-        if (!userId) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-        if (!data.title || !data.description || !data.priority || !data.deadline) {
-            return res.status(400).json({ message: "All details required" });
-        }
+        const userId = req.currentUser!.id;
         const task = await taskService.addTask(data, userId);
         res.status(201).json(task);
     }
