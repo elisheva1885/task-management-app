5f3d7551ebeec94acc9e78acf527d34970dec960
elisheva1885 <elisheva1885@gmail.com>
2026-04-12 11:08:01 +0300

    on add and update task - the date can be of YYYY-MM-DD and not need to be timestemps

--- src/schemas/task.schema.ts
+++ src/schemas/task.schema.ts
@@ -1,17 +1,30 @@
 import z from 'zod'
 import { Priority } from '../constants/priority.js'
+
+const dateSchema = z.string()
+  .refine((value) => /^\d{4}-\d{2}-\d{2}$/.test(value), {
+    message: "Invalid date format. Expected YYYY-MM-DD",
+  })
+  .transform((value) => {
+    const [year, month, day] = value.split("-").map(Number);
+    if(year==undefined || month==undefined || day==undefined){
+       return "Invalid date format. Expected YYYY-MM-DD" ;
+    }
+    const date = new Date(Date.UTC(year, month - 1, day));
+    return date.getTime();
+  });
+
 export const createTaskSchema = z.object({
-	title: z.string(),
-	description: z.string(),
-	priority: z.enum(Priority),
-	deadline: z.string().datetime(),
+    title: z.string(),
+    description: z.string(),
+    priority: z.enum(Priority),
+    deadline: dateSchema,
 })
 
 export const updateTaskSchema = z.object({
     title: z.string(),
-    description : z.string(),
-    priority : z.enum(Priority),
-    deadline : z.string().datetime()
+    description: z.string(),
+    priority: z.enum(Priority),
+    deadline: dateSchema
 }).partial();
-
 
